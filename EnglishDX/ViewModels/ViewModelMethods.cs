2dfd4d3b7646fdbba10bc38ed959a8644ee0929c
kozhevnikov.andrey <andrey.kozhevnikov@devexpress.com>
2025-07-23 11:32:02 +0400

    comp name

--- EnglishDX/ViewModels/ViewModelMethods.cs
+++ EnglishDX/ViewModels/ViewModelMethods.cs
@@ -50,13 +50,13 @@ namespace EnglishDX {
         private static void ConnectToDataBase() {
             string machineName = System.Environment.MachineName;
             if (IsTestMode) {
-                if (machineName == "KOZHEVNIKOV-NBX")
+                if (machineName == "KOZHEVNIKOV-NBY")
                     generalEntity = new EngBaseEntities1("EngBaseEntitiesWorkTest");
                 else
                     generalEntity = new EngBaseEntities1("EngBaseEntitiesHomeTest");
                 return;
             }
-            if (machineName == "KOZHEVNIKOV-NBX") {
+            if (machineName == "KOZHEVNIKOV-NBY") {
                 generalEntity = new EngBaseEntities1("EngBaseEntitiesWork");
             }
             else {
