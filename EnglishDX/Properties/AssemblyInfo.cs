38e0a1c3fa0b522038506857dcc4244c2dfd6169
kozhevnikov.andrey <14300209+AndreyKozhevnikov@users.noreply.github.com>
2026-09-17 17:15:19 +0400

    upd version

--- EnglishDX/Properties/AssemblyInfo.cs
+++ EnglishDX/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("25.1.10.0926")]
-[assembly: AssemblyVersion("25.1.10.0926")]
-[assembly: AssemblyFileVersion("25.1.10.0926")]
+// [assembly: AssemblyVersion("25.9.1.0909")]
+[assembly: AssemblyVersion("25.9.1.0909")]
+[assembly: AssemblyFileVersion("25.9.1.0909")]
